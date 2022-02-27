58d2783ccbaee5bb0fe32edf55dc0292682e5347
Hyesu Choi <chlgotn0580@naver.com>
2022-02-27 11:22:47 +0900

    header

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// 스페셜 오퍼 swiper 
 let swiper = new Swiper(".mySwiper", {
     direction: "vertical",
     slidesPerView: 1,
@@ -9,7 +10,7 @@ let swiper = new Swiper(".mySwiper", {
     },
   });
 
-
+// 객실 미리보기 swiper 
   var swiper2 = new Swiper(".rooms-swiper", {
     navigation: {
       nextEl: ".swiper-button-next",
@@ -18,16 +19,28 @@ let swiper = new Swiper(".mySwiper", {
   });
 
 
-  $('.header-bottom').hover(function(){
+  // 데스크탑, 태블릿 메뉴 온오프
+  $('.nav').hover(function(){
     $('.sub-menu').stop().slideDown();
   }, function(){
     $('.sub-menu').stop().slideUp();
   });
 
 
-  // $('nav > ul > li').hover(function(){
-  //   $(this).find('.sub-menu').stop().slideDown();
+
+// 모바일 영역 메뉴 온오프 
+  $('.menu-bar').click(function(){
+    $('.bx-menu, .bx-x').toggleClass('open');
+    $('.nav-mob').toggleClass('open-menu');
+  })
+
+
+  // //모바일 서브메뉴 온오프
+  // $('.nav-mob>ul>li').hover(function(){
+  //   $(this).find('.sub-menu-mob').stop().slideDown();
   // }, function(){
-  //   $('.sub-menu').stop().slideUp();
+  //   $(this).find('.sub-menu-mob').stop().slideUp();
   // });
+
+
 
